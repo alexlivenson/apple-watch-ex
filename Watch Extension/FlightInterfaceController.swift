19db804497b6e0e5fec7b37e050708aaef23a5d7
alex livenson <alexlivenson1@alexs-MacBook-Pro.local>
2016-02-05 00:01:58 -0800

    Finished part one of apple watch tutorial

---
+++ Watch Extension/FlightInterfaceController.swift
@@ -0,0 +1,59 @@
+//
+//  FlightInterfaceController.swift
+//  AirAber
+//
+//  Created by alex livenson on 2/4/16.
+//  Copyright © 2016 Mic Pringle. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+class FlightInterfaceController: WKInterfaceController {
+    
+    @IBOutlet var flightLabel: WKInterfaceLabel!
+    @IBOutlet var routeLabel: WKInterfaceLabel!
+    @IBOutlet var boardingLabel: WKInterfaceLabel!
+    @IBOutlet var boardTimeLabel: WKInterfaceLabel!
+    @IBOutlet var statusLabel: WKInterfaceLabel!
+    @IBOutlet var gateLabel: WKInterfaceLabel!
+    @IBOutlet var seatLabel: WKInterfaceLabel!
+    
+    var flight: Flight? {
+        didSet {
+            if let flight = flight {
+                flightLabel.setText("Flight \(flight.shortNumber)")
+                routeLabel.setText(flight.route)
+                boardingLabel.setText("\(flight.number) Boards")
+                boardTimeLabel.setText(flight.boardsAt)
+                
+                if flight.onSchedule {
+                    statusLabel.setText("On Time")
+                } else {
+                    statusLabel.setText("Delayed")
+                    statusLabel.setTextColor(UIColor.redColor())
+                }
+                gateLabel.setText("Gate \(flight.gate)")
+                seatLabel.setText("Seat \(flight.seat)")
+            }
+        }
+    }
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+        flight = Flight.allFlights().first!
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
